df6537e0ba7941c3429d457ad9059596e9fd6133
Jonathan L Meek <jonathan.l.meek@gmail.com>
2015-09-09 22:00:08 -0400

    Updated to using the gradle dev tool

---
+++ GraphCreator/app/src/main/java/com/jmeek/graphcreator/InputChoiceActivity.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of GraphCreator.
+
+    GraphCreator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GraphCreator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GraphCreator.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (C) 2012-2015 Jonathan L. Meek
+ */
+
+package com.jmeek.graphcreator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class InputChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inputchoice);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_input_choice, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void startUrlActivity(View v){
+        Intent intent = new Intent(this, GraphCreatorActivity.class);
+        startActivity(intent);
+    }
+
+}
