663facbeadafbbe5e10a810c9180568fedd6a943
Jonathan L. Meek <lemuelmeek@gmail.com>
2015-12-31 14:11:49 -0500

    Updated to using the gradle dev tool and appears to be working properly.

--- GraphCreator/app/src/main/java/com/jmeek/graphcreator/InputChoiceActivity.java
+++ GraphCreator/app/src/main/java/com/jmeek/graphcreator/InputChoiceActivity.java
@@ -22,8 +22,6 @@ package com.jmeek.graphcreator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class InputChoiceActivity extends AppCompatActivity {
@@ -34,27 +32,7 @@ public class InputChoiceActivity extends AppCompatActivity {
         setContentView(R.layout.inputchoice);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_input_choice, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     public void startUrlActivity(View v){
         Intent intent = new Intent(this, GraphCreatorActivity.class);
         startActivity(intent);
